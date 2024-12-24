8322590d8a282b534143652d9dd5d03940ed9cc1
barcojack <yateejhonandres@gmail.com>
2024-12-23 21:32:28 -0500

    final

--- movie-app/server/server.js
+++ movie-app/server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes); // Verifica que este prefijo esté correcto
+app.use('/api/auth', authRoutes); 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
